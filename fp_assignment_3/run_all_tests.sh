59afeccdb8e1720d3c86945e5ab68be75d724fd6
joepeck02 <joepeck02@683c42de-a4a1-11de-b195-ab392a2a73f5>
2009-09-30 02:50:02 +0000

    Run All Tests runs Verbose
    git-svn-id: https://rit-functional.googlecode.com/svn/trunk@13 683c42de-a4a1-11de-b195-ab392a2a73f5

--- fp_assignment_3/run_all_tests.sh
+++ fp_assignment_3/run_all_tests.sh
@@ -3,7 +3,7 @@
 # Date: Saturday, September 26, 2009
 # Description: Run All Haskell Tests
 
-./test_runner Tester.lhs
-./test_runner Subset.lhs
-./test_runner Select.lhs
-./test_runner Matrix.lhs
+./test_runner -v Tester.lhs
+./test_runner -v Subset.lhs
+./test_runner -v Select.lhs
+./test_runner -v Matrix.lhs
